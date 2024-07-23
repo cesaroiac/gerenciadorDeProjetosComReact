77c335d3d1776ac2d0d63a79c93d270faee1e02d
cesaroiac <caiocesarama@gmail.com>
2024-07-22 23:35:36 -0300

    Configurações básicas de rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import Company from './pages/Company';
+import Contact from './pages/Contact';
+import NewProject from './pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Show</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
